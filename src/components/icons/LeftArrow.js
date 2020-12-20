10a38bca72a353d98a1f5971bdb6ce9ec6d1e0b9
alex-kurkov <alexkourkov@yandex.ru>
2020-12-20 17:07:14 +0500

    made carousel els alive

--- src/components/icons/LeftArrow.js
+++ src/components/icons/LeftArrow.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import styled, {keyframes} from 'styled-components/macro';
+
+const motion = keyframes`
+  from {
+  }
+
+  to {
+    transform: translateX(-10px);
+  }
+`;
+const StyledSvg = styled.svg`
+  &:hover {
+    animation: ${motion} .5s ease-out alternate infinite;
+  }
+`;
+
+
 
 const LeftArrow = ( { fill } ) => (
-    <svg width="100%" height="100%" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <StyledSvg width="100%" height="100%" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M37 48L14 25L37 2" stroke={fill} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
+    </StyledSvg>
 );
 
 export default LeftArrow;
--- src/components/icons/RightArrow.js
+++ src/components/icons/RightArrow.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import styled, {keyframes} from 'styled-components/macro';
+
+const motion = keyframes`
+  from {
+  }
+
+  to {
+    transform: translateX(10px);
+  }
+`;
+const StyledSvg = styled.svg`
+  &:hover {
+    animation: ${motion} .5s ease-out alternate infinite;
+  }
+`;
 
 const RightArrow = ( { fill } ) => (
-    <svg width="100%" height="100%" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <StyledSvg width="100%" height="100%" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M14 2L37 25L14 48" stroke={fill} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
+    </StyledSvg>
 );
 
 export default RightArrow;
